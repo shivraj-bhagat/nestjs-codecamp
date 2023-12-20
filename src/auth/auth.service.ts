ae6314b61d3ecd2bbb2c9ac0990542ec01159d99
Shiv Raj Bhagat <shivraj.bhagat@crownstack.com>
2023-12-20 15:07:14 +0530

    added dto for user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,36 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable({})
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+@Injectable()
 export class AuthService {
+  constructor(private prismaService: PrismaService) {}
+
   login() {
     return {
       msg: 'hi, login',
     };
   }
 
-  signup() {
-    return {
-      msg: 'hi, sign up',
-    };
+  async signup(dto: AuthDto) {
+    try {
+      const hash = await argon.hash(dto.password);
+      const user = await this.prismaService.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException('Email Already Exists');
+        }
+      }
+      throw err;
+    }
   }
 }
