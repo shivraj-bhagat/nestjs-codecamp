fe7c3347af02afea7d81a50b1251028672297cc4
Shiv Raj Bhagat <shivraj.bhagat@crownstack.com>
2023-12-22 14:58:47 +0530

    working with guards

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,14 +3,26 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class AuthService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
-  login() {
-    return {
-      msg: 'hi, login',
-    };
+  async login(dto: AuthDto) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) throw new ForbiddenException("Credentails didn't match");
+    const isPWMatched = await argon.verify(user.hash, dto.password);
+    if (!isPWMatched) throw new ForbiddenException("Password didn't match");
+    return this.token(user.id, user.email);
   }
 
   async signup(dto: AuthDto) {
@@ -22,8 +34,7 @@ export class AuthService {
           hash,
         },
       });
-      delete user.hash;
-      return user;
+      return this.token(user.id, user.email);
     } catch (err) {
       if (err instanceof PrismaClientKnownRequestError) {
         if (err.code === 'P2002') {
@@ -32,5 +43,24 @@ export class AuthService {
       }
       throw err;
     }
+  }
+
+  async token(
+    userId: number,
+    email: string,
+  ): Promise<{ token: string; id: number }> {
+    const data = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+    const generatedToken = await this.jwt.signAsync(data, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+    return {
+      id: userId,
+      token: generatedToken,
+    };
   }
 }
---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JWTStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+
+  validate(payload: any) {
+    return payload;
+  }
+}
